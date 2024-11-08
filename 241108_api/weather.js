3d3099ca81059e652c43b04bac0892f7dab77a50
Juan Lee <ninejuan@protonmail.com>
2024-11-08 12:27:40 +0900

    add: gasanjeom 241108

--- 241108_api/weather.js
+++ 241108_api/weather.js
@@ -1,18 +1,31 @@
 const button = document.querySelector(".button");
-const API_KEY = "";
+const API_KEY = "14f7e52e4618132753334bb20aa84d0c";
 const tempSection = document.querySelector(".temperature");
 const placeSection = document.querySelector(".place");
 const iconSection = document.querySelector(".icon");
 const descSection = document.querySelector(".description");
+const lat = document.querySelector("#lat");
+const lon = document.querySelector("#lon");
+
+const setLoc = () => {
+  const setValue = (position) => {
+    lat.value = position.coords.latitude;
+    lon.value = position.coords.longitude;
+  };
+  const fail = (e) => {
+    alert(e);
+  };
+  navigator.geolocation.getCurrentPosition(setValue, fail);
+};
 
 button.addEventListener("click", () => {
   navigator.geolocation.getCurrentPosition(success, fail);
 });
 
 const success = (position) => {
-  const latitude = position.coords.latitude;
-  const longitude = position.coords.longitude;
-
+  const latitude = lat.value.length > 0 ? lat.value : position.coords.latitude;
+  const longitude =
+    lon.value.length > 0 ? lon.value : position.coords.longitude;
   getWeather(latitude, longitude);
 };
 
